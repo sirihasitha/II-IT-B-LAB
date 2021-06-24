2e927b0479e57a2265d7945c176cdc9b36f7750b
sirihasitha <81104669+sirihasitha@users.noreply.github.com>
2021-06-24 03:31:31 -0700

    Add files via upload

---
+++ JP LAB/Array Programs/charArraytoString.java
@@ -0,0 +1,10 @@
+class charArraytoString
+{
+    public static void main(String[] args)
+    {
+        char[] charArray = { 'J', 'a', 'v', 'a' };
+ 
+        String str = new String(charArray);
+        System.out.println(str);
+    }
+}
