085fcb263fcdba505406cc02e7e2113b052f6f4b
sirihasitha <81104669+sirihasitha@users.noreply.github.com>
2021-06-24 03:25:51 -0700

    Add files via upload

---
+++ JP LAB/Basic Programs/evenorodd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class evenorodd{
+public static void main(String args[]){
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first number");
+a=sc.nextInt();
+if(a%2 == 0){
+System.out.println("EVEN");}
+else{
+System.out.println("ODD");}}}
---
+++ JP LAB/Basic Programs/multiply.java
@@ -0,0 +1,5 @@
+public class multiply{
+public static void main(String args[]){
+int a =3,b = 8;
+multiply obj = new multiply();
+System.out.println(+(a*b));}}
